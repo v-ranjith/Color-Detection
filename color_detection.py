6383fb8542935dedc25527be42ed543822454498
v-ranjith <75880644+v-ranjith@users.noreply.github.com>
2020-12-12 18:22:41 +0530

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -1,18 +1,23 @@
 # pip install pandas opencv-python
-# visit pyGuru on youtube
 
 import cv2
 import pandas as pd
 import numpy as np
 
 # --------------------------------------------------------------------------
-#code modified by Ranjith V
+# source - pyGuru on youtube
+# code modified by Ranjith V
 
-img_path = 'colorpic.jpg'
+#path of the sample image in which the colors are to be detected
+img_path = 'sample1.jpg'
+#path of the .csv file where the details of the colors such as color name, color code, RGB values are stored
 csv_path = 'colors.csv'
+
+#our .csv file contains 865 colors
 
 # reading csv file
 index = ['color', 'color_name', 'hex', 'R', 'G', 'B']
+#DataFrame
 df = pd.read_csv(csv_path, names=index, header=None)
 
 # reading image
@@ -21,7 +26,7 @@ img = cv2.imread(img_path)
 img = cv2.resize(img, (800,600))
 
 #declaring global variables
-clicked = False
+moved = False
 r = g = b = xpos = ypos = 0
 
 #function to calculate minimum distance from all colors and get the most matching color
@@ -34,11 +39,11 @@ def get_color_name(R,G,B):
 			cname = df.loc[i, 'color_name']
 	return cname
 
-#function to get x,y coordinates of mouse double click
+#function to get x,y coordinates upon the movement of the cursor
 def draw_function(event, x, y, flags, params):
 	if event == cv2.EVENT_MOUSEMOVE:
-		global b, g, r, xpos, ypos, clicked
-		clicked = True
+		global b, g, r, xpos, ypos, moved
+		moved = True
 		xpos = x
 		ypos = y
 		b,g,r = img[y,x]
@@ -46,32 +51,34 @@ def draw_function(event, x, y, flags, params):
 		g = int(g)
 		r = int(r)
 
-# creating window
+# creating windows
 cv2.namedWindow('Image')
 cv2.namedWindow('Color Details')
 cv2.setMouseCallback('Image', draw_function)
 
-
+# continuous execution of the program until Esc key is pressed
 while True:
 	cv2.imshow('Image', img)
 	cv2.imshow('Color Details', blnk_img)
-	if clicked:
-		#cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
+	if moved:
+		#cv2.rectangle(image, startpoint, endpoint, color, 		thickness)
 		cv2.rectangle(blnk_img, (20,20), (700,100), (255,255,255), -1)
+		#	Note: thickness = -1 fills entire rectangle 
 
 		#Creating text string to display( Color name and RGB values )
+		#Color Name
 		text1 = get_color_name(r,g,b)
+		#RGB Values
 		text2 = 'R = ' + str(r) + ' G = ' + str(g) + ' B = ' + str(b)
-		#cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-		cv2.putText(blnk_img, text1, (50,50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
-		cv2.putText(blnk_img, text2, (50,90), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
 
-		#For very light colours we will display text in black colour
-		# if r+g+b >=600:
-		# 	cv2.putText(blnk_img, text1, (50,50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
-		# 	cv2.putText(blnk_img, text2, (50,90), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
+		#displaying the Color name and RGB values in 2 different lines
+		#cv2.putText(img,		text,	start,		font(0-7),	fontScale,	color,	thickness,	lineType )
+		cv2.putText(blnk_img,	text1,	(50,50),	2,			0.8,		(0,0,0),	2,		cv2.LINE_AA)
+		cv2.putText(blnk_img,	text2,	(50,90),	2,			0.8,		(0,0,0),	2,		cv2.LINE_AA)
 
+	#Program termination when Esc key is pressed	
 	if cv2.waitKey(20) & 0xFF == 27:
 		break
 
+#destroys all windows
 cv2.destroyAllWindows()
